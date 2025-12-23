1de63689410988d36547d97606f81f49ae006733
HappyTanuki <rlaalstn120@icloud.com>
2025-12-23 14:41:13 +0900

    ReadWritable 인터페이스 추가

--- include/common/interfaces.h
+++ include/common/interfaces.h
@@ -1,10 +1,32 @@
 #ifndef BEDROCK_COMMON_COMMON_INTERFACES_H_
 #define BEDROCK_COMMON_COMMON_INTERFACES_H_
 
+#include <array>
+#include <cstdint>
+#include <span>
+
+#include "types_enums.h"
+
+namespace bedrock {
+
 class Validatable {
  public:
-  virtual bool IsValid() const = 0;
   virtual ~Validatable() = default;
+
+  virtual bool IsValid() const = 0;
 };
+
+template <std::uint32_t BufferSize, typename StatusType>
+  requires StatusEnum<StatusType>
+class ReadWritable {
+ public:
+  virtual ~ReadWritable() = default;
+
+  virtual DataWithStatus<std::array<std::byte, BufferSize>, StatusType>
+  Read() = 0;
+  virtual StatusType Write(std::span<std::byte> data) = 0;
+};
+
+}  // namespace bedrock
 
 #endif
