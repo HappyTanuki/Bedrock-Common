7f847cb021c18561f6e697c3627fb155cdd81592
HappyTanuki <rlaalstn120@icloud.com>
2025-12-25 17:08:07 +0900

    인터페이스 수정

--- include/common/interfaces.h
+++ include/common/interfaces.h
@@ -24,7 +24,7 @@ class ReadWritable {
 
   virtual DataWithStatus<std::pair<std::vector<std::byte>, std::uint32_t>,
                          StatusType>
-  Read() = 0;
+  Read(std::uint32_t request_size) = 0;
   virtual StatusType Write(std::span<std::byte> data) = 0;
 };
 
